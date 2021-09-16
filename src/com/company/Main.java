ac0f17185b2ee03323e1fb02cc5d5e1b76531649
till <till@spsmb.cz>
2021-09-16 14:54:56 +0200

    Zmena na nauceni programovani a siti

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,27 +5,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        /* Naucim se programovani a site */
+        /* Naucim se programovani a site lol, lololol */
         Scanner sc = new Scanner(System.in);
         System.out.println("Zadejte prvni cislo: ");
         double a = sc.nextDouble();
         System.out.println("Zadejte druhe cislo: ");
         double b = sc.nextDouble();
         switch (args[0]) {
-            case "s":
-                System.out.println(a + b);
-                break;
-            case "o":
-                System.out.println(a - b);
-                break;
-            case "n":
-                System.out.println(a * b);
-                break;
-            case "d":
-                System.out.println(a / b);
-                break;
-            default:
-                System.out.println("Neplatny argument");
+            case "s" -> System.out.println(a + b);
+            case "o" -> System.out.println(a - b);
+            case "n" -> System.out.println(a * b);
+            case "d" -> System.out.println(a / b);
+            default -> System.out.println("Neplatny argument");
         }
     }
 
