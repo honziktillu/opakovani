a5227bbbe49fe6d9d9ed349ec245d0c37e3dcfd5
Jan Till <72889363+honziktillu@users.noreply.github.com>
2021-09-16 14:42:24 +0200

    Update Main.java
    Added comment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        /* Naucim se site */
         Scanner sc = new Scanner(System.in);
         System.out.println("Zadejte prvni cislo: ");
         double a = sc.nextDouble();
